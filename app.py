79303805d0fdb417d59fed3308d828c4d9ced1bc
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-03-30 12:17:45 -0400

    new templates was added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route('/login')
 def login():
     return render_template ("login.html")
-    
+
 @app.route('/about/')
 def about():
     return render_template ("about.html")
@@ -31,6 +31,27 @@ def editstorage():
 def deletestorage():
   return render_template("deletestorage.html")
 
+#Show a Storage container
+@app.route('/storage/')
+@app.route('/storage/container')
+def showContainer():
+  return render_template("container.html")
+
+#Create a new item in the container
+@app.route('/storage/container/new')
+def newContainerItem():
+  return render_template("newcontaineritem.html")
+
+#Edit an item in the container
+@app.route('/storage/container/edit')
+def editContainerItem():
+  return render_template("editcontaineritem.html")
+
+
+#Delete an item in the container
+@app.route('/storage/container/delete')
+def deleteContainerItem():
+  return render_template("deletecontaineritem.html")
 
 
 if __name__=="__main__":
