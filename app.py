677d23fbfbb59cdddf6a3f7af80c33a0f4e7cd94
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-03-30 20:35:38 -0400

    app.py started but http://0.0.0.0:5000/ return error

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-app=Flask(__name__)
-
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Storage, ContainerItem
+
+app = Flask(__name__)
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///storage.db')
@@ -12,20 +12,24 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 @app.route('/login')
 def login():
-    return render_template ("login.html")
+    return render_template('login.html')
+
 
 @app.route('/about/')
 def about():
-    return render_template ("about.html")
+    return render_template('about.html')
+
 
 #Show all Storages
 @app.route('/')
 @app.route('/storage/')
 def showStorages():
     storages = session.query(Storage).order_by(asc(Storage.name))
-    return render_template("storages.html", storages = storages)
+    return render_template('storages.html', storages=storages)
+
 
 #Create a new Storage
 @app.route('/storage/new', methods=['GET','POST'])
@@ -50,19 +54,21 @@ def editStorage(storage_id):
         flash('Storage Successfully Edited %s' % editstorage.name)
         return redirect(url_for('showstorages'))
     else:
-    return render_template("editstorage.html", storage = editstorage)
+        return render_template('editstorage.html', storage = editstorage)
+
 
 #Delete a Storage
 @app.route('/storage/<int:storage_id>/delete/', methods = ['GET','POST'])
 def deleteStorage(storage_id):
     storagetodelete = session.query(Storage).filter_by(id = storage_id).one()
-  if request.method == 'POST':
-    session.delete(storagetodelete)
-    flash('%s Successfully Deleted' % storagetodelete.name)
-    session.commit()
-    return redirect(url_for('showstorages', storage_id = storage_id))
-else:
-    return render_template("deletestorage.html", storage = storagetodelete)
+    if request.method == 'POST':
+        session.delete(storagetodelete)
+        flash('%s Successfully Deleted' % storagetodelete.name)
+        session.commit()
+        return redirect(url_for('showstorages', storage_id = storage_id))
+    else:
+        return render_template('deletestorage.html', storage = storagetodelete)
+
 
 #Show a Storage container
 @app.route('/storage/<int:storage_id>/')
@@ -72,18 +78,20 @@ def showContainer(storage_id):
     items = session.query(ContainerItem).filter_by(storage_id = storage_id).all()
     return render_template("container.html", items = items, storage = storage)
 
+
 #Create a new item in the container
 @app.route('/storage/<int:storage_id>/container/new', methods=['GET','POST'])
 def newContainerItem(storage_id):
     storage = session.query(Storage).filter_by(id = storage_id).one()
-  if request.method == 'POST':
-      newItem = ContainerItem(name = request.form['name'], description = request.form['description'], storage_id = storage_id)
-      session.add(newItem)
-      session.commit()
-      flash('New Container %s Item Successfully Created' % (newItem.name))
-      return redirect(url_for('showContainer', storage_id = storage_id))
-  else:
-    return render_template("newcontaineritem.html", storage_id = storage_id)
+    if request.method == 'POST':
+        newItem = ContainerItem(name = request.form['name'], description = request.form['description'], storage_id = storage_id)
+        session.add(newItem)
+        session.commit()
+        flash('New Container %s Item Successfully Created' % (newItem.name))
+        return redirect(url_for('showContainer', storage_id = storage_id))
+    else:
+        return render_template('newcontaineritem.html', storage_id = storage_id)
+
 
 #Edit an item in the container
 @app.route('/storage/<int:storage_id>/container/<int:container_id>/edit',  methods=['GET','POST'])
@@ -118,4 +126,5 @@ def deleteContainerItem(storage_id, container_id):
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
