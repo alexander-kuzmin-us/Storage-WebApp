e4d4169b26512306fc176264507c89e3e1b29e6a
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-04-03 21:52:57 -0400

    secret deleted

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ import requests
 
 app = Flask(__name__)
 
-# Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'_5#y2L"F4Q8z\n\xecdeppp//33]/'
-
 
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
