cc628036eed986975f968e4d73d1e82848bf6ae2
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-04-11 11:29:14 -0400

    google login added

--- application.py
+++ application.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-from sqlalchemy import create_engine, asc
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Storage, ContainerItem
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask_oauth import OAuth
 from flask import session as login_session
 import random
 import string
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Storage, ContainerItem
+
+
+# You must configure these 3 values from Google APIs console
+# https://code.google.com/apis/console
+GOOGLE_CLIENT_ID = '462283270013-4l8lmtsrefrernp61ipu1s89tqqbsdcd.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'SHk33xIo-L7PiPQMLQ74-0_2'
+REDIRECT_URI = '/oauth2callback'  # one of the Redirect URIs from Google APIs console
+
+SECRET_KEY = 'development key'
+DEBUG = True
 
 
 app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
 
 
 #Connect to Database and create database session
@@ -18,13 +32,56 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# Create anti-forgery state token
+google = oauth.remote_app('google',
+base_url='https://www.google.com/accounts/',
+authorize_url='https://accounts.google.com/o/oauth2/auth',
+request_token_url=None,
+request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+'response_type': 'code'},
+access_token_url='https://accounts.google.com/o/oauth2/token',
+access_token_method='POST',
+access_token_params={'grant_type': 'authorization_code'},
+consumer_key=GOOGLE_CLIENT_ID,
+consumer_secret=GOOGLE_CLIENT_SECRET)
+
+@app.route('/')
+def index():
+access_token = session.get('access_token')
+if access_token is None:
+return redirect(url_for('login'))
+
+access_token = access_token[0]
+from urllib2 import Request, urlopen, URLError
+
+headers = {'Authorization': 'OAuth '+access_token}
+req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+None, headers)
+try:
+res = urlopen(req)
+except URLError, e:
+if e.code == 401:
+# Unauthorized - bad token
+session.pop('access_token', None)
+return redirect(url_for('login'))
+return res.read()
+
+return res.read()
+
 @app.route('/login')
-def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
-    login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+def login():
+callback=url_for('authorized', _external=True)
+return google.authorize(callback=callback)
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+access_token = resp['access_token']
+session['access_token'] = access_token, ''
+return redirect(url_for('index'))
+
+@google.tokengetter
+def get_access_token():
+return session.get('access_token')
 
 
 #JSON APIs to view storage Information
