69649193c33ac344e41d25e9c78521d4bde0f47c
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2019-09-02 20:32:10 -0400

    Some files were modified for PEP8 requirements

--- app.py
+++ app.py
@@ -23,14 +23,11 @@ import json
 from flask import make_response
 import requests
 
-
 app = Flask(__name__)
-
 
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Auto Repair Center"
-
 
 #  #Connect to Database and create database session
 engine = create_engine('sqlite:///autorepaircenter.db',
@@ -39,23 +36,22 @@ engine = create_engine('sqlite:///autorepaircenter.db',
                        )
 Base.metadata.bind = engine
 
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
 # Create anti-forgery state token
 @app.route('/login')
-def showLogin():
+def show_login():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
+                    for x in x.range(32))
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
 
 @app.route('/gconnect', methods=['POST'])
-def gconnect():
+def g_connect():
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -99,7 +95,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print ("Token's client ID does not match app's.")
+        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -107,7 +103,7 @@ def gconnect():
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
         response = make_response(
-                json.dumps('Current user is already connected.'), 200)
+            json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -127,9 +123,9 @@ def gconnect():
     login_session['email'] = data['email']
 
     # see if user exists, if it doesn't make a new one
-    user_id = getUserID(login_session['email'])
+    user_id = get_user_id(login_session['email'])
     if not user_id:
-        user_id = createUser(login_session)
+        user_id = create_user(login_session)
     login_session['user_id'] = user_id
 
     output = ''
@@ -143,28 +139,28 @@ def gconnect():
                 150px;-webkit-border-radius: \
                 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
-    print ("done!")
+    print("done!")
     return output
 
 
 # User Helper Functions
-def createUser(login_session):
-    newUser = User(
+def create_user(login_session):
+    new_user = User(
         name=login_session['username'],
         email=login_session['email'],
         picture=login_session['picture'])
-    session.add(newUser)
+    session.add(new_user)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
 
-def getUserInfo(user_id):
+def get_user_info(user_id):
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
-def getUserID(email):
+def get_user_id(email):
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
@@ -174,24 +170,24 @@ def getUserID(email):
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
-def gdisconnect():
+def g_disconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
-        print ('Access Token is None')
+        print('Access Token is None')
         response = make_response(
-                json.dumps(
-                    'Current user not connected.'), 401)
+            json.dumps(
+                'Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print ('In gdisconnect access token is %s', access_token)
-    print ('User name is: ')
-    print (login_session['username'])
+    print('In gdisconnect access token is %s', access_token)
+    print('User name is: ')
+    print(login_session['username'])
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' \
-        % login_session['access_token']
+          % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print ('result is ')
-    print (result)
+    print('result is ')
+    print(result)
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
@@ -211,10 +207,10 @@ def gdisconnect():
 
 # JSON APIs to view AutoRepairCenter Information
 @app.route('/autorepaircenter/<int:autorepaircenter_id>/container/JSON')
-def AutorepairCentercenterContainerJSON(autorepaircenter_id):
+def autorepair_centercenter_container_json(autorepaircenter_id):
     autorepaircenter = session \
-            .query(AutoRepairCenter) \
-            .filter_by(id=autorepaircenter_id).one()
+        .query(AutoRepairCenter) \
+        .filter_by(id=autorepaircenter_id).one()
     items = session \
         .query(ContainerItem) \
         .filter_by(autorepaircenter_id=autorepaircenter_id).all()
@@ -224,15 +220,15 @@ def AutorepairCentercenterContainerJSON(autorepaircenter_id):
 @app.route(
     '/autorepaircenter/<int:autorepaircenter_id>/\
                 container/<int:container_id>/JSON')
-def ContainerItemJSON(autorepaircenter_id, container_id):
-    Container_Item = session \
-            .query(ContainerItem) \
-            .filter_by(id=container_id).one()
-    return jsonify(Container_Item=Container_Item.serialize)
+def container_item_json(autorepair_center_id, container_id):
+    container_item = session \
+        .query(ContainerItem) \
+        .filter_by(id=container_id).one()
+    return jsonify(Container_Item=container_item.serialize)
 
 
 @app.route('/autorepaircenter/JSON')
-def AutoRepairCentercentersJSON():
+def autorepair_centercenters_json():
     autorepaircenters = session.query(AutoRepairCenter).all()
     return jsonify(autorepaircenters=[r.serialize for r in autorepaircenters])
 
@@ -240,9 +236,9 @@ def AutoRepairCentercentersJSON():
 # Show all Autorepair centercenters
 @app.route('/')
 @app.route('/autorepaircenter/')
-def showAutoRepairCenters():
+def show_auto_repair_centers():
     autorepaircenters = session \
-            .query(AutoRepairCenter).order_by(asc(AutoRepairCenter.name))
+        .query(AutoRepairCenter).order_by(asc(AutoRepairCenter.name))
     if 'username' not in login_session:
         return render_template(
             'publicautorepaircenters.html',
@@ -254,13 +250,13 @@ def showAutoRepairCenters():
 
 # Create a new Autorepair centercenter
 @app.route('/autorepaircenter/new/', methods=['GET', 'POST'])
-def newAutoRepairCenter():
+def new_auto_repair_center():
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
         newAutoRepairCenter = AutoRepairCenter(
-                            name=request
-                            .form['name'], user_id=login_session['user_id'])
+            name=request
+                .form['name'], user_id=login_session['user_id'])
         session.add(newAutoRepairCenter)
         flash(
             'New Auto Repair Center %s Successfully Created' %
@@ -276,7 +272,7 @@ def newAutoRepairCenter():
 @app.route(
     '/autorepaircenter/<int:autorepaircenter_id>/edit/',
     methods=['GET', 'POST'])
-def editAutoRepairCenter(autorepaircenter_id):
+def edit_auto_repair_center(autorepaircenter_id):
     editedAutoRepairCenter = session.query(
         AutoRepairCenter).filter_by(id=autorepaircenter_id).one()
     if 'username' not in login_session:
@@ -307,36 +303,36 @@ def editAutoRepairCenter(autorepaircenter_id):
 @app.route(
     '/autorepaircenter/<int:autorepaircenter_id>/delete/',
     methods=['GET', 'POST'])
-def deleteAutoRepairCenter(autorepaircenter_id):
+def delete_autorepair_center(autorepaircenter_id):
     if 'username' not in login_session:
         return redirect('/login')
-    autorepaircenterToDelete = session.query(
+    autorepaircenter_to_delete = session.query(
         AutoRepairCenter).filter_by(id=autorepaircenter_id).one()
-    if autorepaircenterToDelete.user_id != login_session['user_id']:
+    if autorepaircenter_to_delete.user_id != login_session['user_id']:
         return "<script>function myFunction() {alert( \
         'You are not authorized to delete this autorepaircenter. \
         Please create your own autorepaircenter in order to delete.' \
         );}</script><body onload='myFunction()''>"
     if request.method == 'POST':
-        session.delete(autorepaircenterToDelete)
-        flash('%s Successfully Deleted' % autorepaircenterToDelete.name)
+        session.delete(autorepaircenter_to_delete)
+        flash('%s Successfully Deleted' % autorepaircenter_to_delete.name)
         session.commit()
         return redirect(url_for(
             'showAutoRepairCenters', autorepaircenter_id=autorepaircenter_id))
     else:
         return render_template(
             'deleteautorepaircenter.html',
-            autorepaircenter=autorepaircenterToDelete)
+            autorepaircenter=autorepaircenter_to_delete)
         session.close()
 
 
 # Show a Autorepair centercenter container
 @app.route('/autorepaircenter/<int:autorepaircenter_id>/')
 @app.route('/autorepaircenter/<int:autorepaircenter_id>/container/')
-def showContainer(autorepaircenter_id):
+def show_container(autorepaircenter_id):
     autorepaircenter = session.query(
         AutoRepairCenter).filter_by(id=autorepaircenter_id).one()
-    creator = getUserInfo(autorepaircenter.user_id)
+    creator = get_user_info(autorepaircenter.user_id)
     items = session.query(ContainerItem).filter_by(
         autorepaircenter_id=autorepaircenter_id).all()
     if 'username' not in login_session or creator.id != \
@@ -356,7 +352,7 @@ def showContainer(autorepaircenter_id):
 @app.route(
     '/autorepaircenter/<int:autorepaircenter_id>/container/new/',
     methods=['GET', 'POST'])
-def newContainerItem(autorepaircenter_id):
+def new_container_item(autorepaircenter_id):
     if 'username' not in login_session:
         return redirect('/login')
     autorepaircenter = session.query(
@@ -387,7 +383,7 @@ def newContainerItem(autorepaircenter_id):
     '/autorepaircenter/<int:autorepaircenter_id>/container/\
     <int:container_id>/edit',
     methods=['GET', 'POST'])
-def editContainerItem(autorepaircenter_id, container_id):
+def edit_container_item(autorepaircenter_id, container_id):
     if 'username' not in login_session:
         return redirect('/login')
     editedItem = session.query(ContainerItem).filter_by(id=container_id).one()
@@ -425,14 +421,14 @@ def editContainerItem(autorepaircenter_id, container_id):
     '/autorepaircenter/<int:autorepaircenter_id>/container/\
     <int:container_id>/delete',
     methods=['GET', 'POST'])
-def deleteContainerItem(autorepaircenter_id, container_id):
+def delete_container_item(autorepaircenter_id, container_id):
     if 'username' not in login_session:
         return redirect('/login')
     autorepaircenter = session.query(
         AutoRepairCenter).filter_by(id=autorepaircenter_id).one()
     itemToDelete = session.query(
         ContainerItem
-        ).filter_by(id=container_id).one()
+    ).filter_by(id=container_id).one()
     if login_session['user_id'] != autorepaircenter.user_id:
         return "<script>function myFunction() {alert('You are not authorized \
         to delete Container items to this Autocenter. Please create your \
