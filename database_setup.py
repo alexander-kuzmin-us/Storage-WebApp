c51fd1dfacb288b720f8d4389d2d6cfc0683b030
Alex Kuzmin <14186600+alexander-kuzmin-us@users.noreply.github.com>
2025-03-16 21:00:09 -0400

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -9,59 +9,73 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id=Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
 
 
 class AutoRepairCenter(Base):
     __tablename__ = 'autorepaircenter'
 
-    id=Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id=Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = relationship(User)
-    container_item = relationship('ContainerItem', cascade='all, delete-orphan')
+    container_item = relationship('ContainerItem', 
+                                 cascade='all, delete-orphan',
+                                 backref='autorepaircenter')
     
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
 
 class ContainerItem(Base):
     __tablename__ = 'container_item'
 
-
-    name = Column(String(80), nullable = False)
-    id=Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
     description = Column(String(250))
     price = Column(String(8))
     type = Column(String(250))
-    autorepaircenter_id=Column(Integer,ForeignKey('autorepaircenter.id'))
-    autorepaircenter = relationship(AutoRepairCenter)
-    user_id=Column(Integer, ForeignKey('user.id'))
+    autorepaircenter_id = Column(Integer, 
+                               ForeignKey('autorepaircenter.id', ondelete='CASCADE'),
+                               nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = relationship(User)
-
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'type'         : self.type,
-           'autorepaircenter_id'    : self.autorepaircenter_id,
-           'user_id'    :   self.user_id,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'type': self.type,
+            'autorepaircenter_id': self.autorepaircenter_id,
+            'user_id': self.user_id
+        }
 
 
+# Create an engine that stores data in the local directory's autorepaircenter.db file
 engine = create_engine('sqlite:///autorepaircenter.db')
 
-
+# Create all tables in the engine
 Base.metadata.create_all(engine)
